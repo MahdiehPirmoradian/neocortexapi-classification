f17ffd978b726da54527ee99450c04cd7f52b07c
Mahdieh Pirmoradian <mahdieh.pirmoradian@stud.fra-uas.de>
2022-03-15 21:26:00 +0100

    Minor Code Editings

--- ImageClassification/ImageClassification/Experiment.cs
+++ ImageClassification/ImageClassification/Experiment.cs
@@ -103,7 +103,7 @@ namespace ConsoleApp
 
             //This Lines are for reading the TestFolder image which is saved By name B.jpg inside of TestFolder
             string MyProjectDir = DirProject();
-            string TestFolder = MyProjectDir + "\\TestFolder\\B.jpg";
+            string TestFolder = MyProjectDir + "\\ImageClassification\\TestFolder\\B.jpg";
             //prediction code
             //Input Image encoding
             int[] encodedInputImage = ReadImageData(TestFolder, width,height);
@@ -355,7 +355,7 @@ namespace ConsoleApp
             cortexLayer.HtmModules.Add("sp", sp);
 
             // Learning process will take 1000 iterations (cycles)
-            int maxSPLearningCycles = 1000;
+            int maxSPLearningCycles = 1;
 
             // Save the result SDR into a list of array
             Dictionary<string, int[]> outputValues = new Dictionary<string, int[]>();
